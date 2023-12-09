eabe5f10eacae6c153aa2ed17e214971ce9496de
anantjadhav <48009462+anantjadhav@users.noreply.github.com>
2023-12-09 12:44:12 +0530

    Create variables.js
    first commit

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+// define variables using following keywords
+
+var firstName = 'Anant';
+let lastName = 'Jadhav';
+const id = 1;
+
+console.log(firstName);
+console.log(lastName);
+console.log(id);
