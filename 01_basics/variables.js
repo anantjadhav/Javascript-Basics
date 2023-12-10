d5639b29ea93807fe2fb5751329e08666a2ce965
anantjadhav <48009462+anantjadhav@users.noreply.github.com>
2023-12-10 10:19:23 +0530

    Update variables.js

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -8,9 +8,4 @@ console.log(firstName);
 console.log(lastName);
 console.log(id);
 
-let myArr1 = [1,2,3,4,5,6];
-let myArr2 = [7,8,9,10,11,12];
 
-let myArr3 = myArr1.concat(myArr2);
-
-console.log(myArr3);
