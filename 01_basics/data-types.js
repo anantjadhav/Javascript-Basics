879e40fe6bd0eed62436ed75eb50fdb71235abac
anantjadhav <48009462+anantjadhav@users.noreply.github.com>
2023-12-11 22:39:58 +0530

    Update data-types.js

--- 01_basics/data-types.js
+++ 01_basics/data-types.js
@@ -24,3 +24,16 @@ let user = {
 
 console.log(name, id, cars, mystatus, myWealth, isLoggedin, bigNumber, user);
 console.table([typeof name, typeof id, typeof cars, typeof mystatus, typeof myWealth, typeof isLoggedin, typeof bigNumber, typeof user])
+
+// INFINITE CURRYING FUNCTION
+
+function add(a) {
+    return function(b) {
+        if(!b) {
+            return a;
+        }
+        return add(a+b);
+    }
+}
+
+console.log(add(100)(200)(300)(400)());
